1c6cdb8787a3aad42b72348e65e5d318f958ff0e
Mridul Seth <seth.mridul@gmail.com>
2021-05-20 16:39:26 +0200

    setup storageclass

--- terraform/rook-ceph-setup/main.tf
+++ terraform/rook-ceph-setup/main.tf
@@ -43,12 +43,37 @@ provider "kubectl" {
   config_path = "../linode-cluster/kube-config"
 }
 
-data "kubectl_file_documents" "manifests" {
+data "kubectl_file_documents" "manifests_cluster" {
     content = file("cluster.yaml")
 }
 
 resource "kubectl_manifest" "ceph-cluster" {
     depends_on   = [helm_release.rook-ceph]
-    count     = length(data.kubectl_file_documents.manifests.documents)
-    yaml_body = element(data.kubectl_file_documents.manifests.documents, count.index)
+    count     = length(data.kubectl_file_documents.manifests_cluster.documents)
+    yaml_body = element(data.kubectl_file_documents.manifests_cluster.documents, count.index)
+}
+
+// Create storage class
+data "kubectl_file_documents" "manifests_storageclass" {
+    content = file("storageclass.yaml")
+}
+resource "kubectl_manifest" "storage_class" {
+    depends_on   = [kubectl_manifest.ceph-cluster]
+    count     = length(data.kubectl_file_documents.manifests_storageclass.documents)
+    yaml_body = element(data.kubectl_file_documents.manifests_storageclass.documents, count.index)
+}
+
+// Hackish way to patch to change default storage
+resource "null_resource" "patch_storage" {
+  depends_on = [kubectl_manifest.storage_class]
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      KUBECONFIG = "../linode-cluster/kube-config"
+    }
+    command = <<EOT
+      kubectl patch storageclass linode-block-storage-retain -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}';
+      kubectl patch storageclass rook-ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+    EOT
+  }
 }
